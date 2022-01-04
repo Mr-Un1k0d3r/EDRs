cf998575c4bb1bc0c8001c6cf2ec49e8c0920527
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2022-01-04 14:08:59 -0500

    Create dumpbytes.c

---
+++ dumpbytes.c
@@ -0,0 +1,42 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <tlhelp32.h>
+
+VOID ListLoadedDlls() {
+
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 0);
+    MODULEENTRY32 me32;
+    me32.dwSize = sizeof(MODULEENTRY32);
+
+    printf("Listing loaded modules inside process PID %d\n------------------------------------------\n", GetCurrentProcessId());
+    if(Module32First(hSnap, &me32)) {
+        do {
+            printf("%s is loaded at 0x%p.\n", me32.szExePath, me32.modBaseAddr);
+
+        } while(Module32Next(hSnap, &me32));
+    }
+
+    CloseHandle(hSnap);
+}
+
+int main(int argc, char **argv) {
+	
+	ListLoadedDlls();
+	
+	DWORD dwSize = atoi(argv[1]);
+	CHAR *dll = argv[2];
+	CHAR *func = argv[3];
+	
+	FARPROC ptr = GetProcAddress(LoadLibrary(dll),func);
+	printf("%s!%s found at 0x%p\n", dll, func, ptr);
+	
+	CHAR *data = ptr;
+	
+	DWORD i = 0;
+	
+	for(i; i < dwSize; i++) {
+		printf("%02x", (unsigned char)data[i]);
+	}
+	
+	return 0;
+}
