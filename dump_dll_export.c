92a05b1fa78a4f8f2762b6e7f2851982edde3b41
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2022-03-28 19:38:38 -0400

    Create dump_dll_export.c

---
+++ dump_dll_export.c
@@ -0,0 +1,35 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <tlhelp32.h>
+#include <winnt.h>
+
+VOID DumpListOfExport(VOID *lib) {
+
+    IMAGE_DOS_HEADER* MZ = (IMAGE_DOS_HEADER*)lib;
+    IMAGE_NT_HEADERS* PE = (IMAGE_NT_HEADERS*)((BYTE*)lib + MZ->e_lfanew);
+    IMAGE_EXPORT_DIRECTORY* export = (IMAGE_EXPORT_DIRECTORY*)((BYTE*)lib + PE->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+    
+    DWORD *name = (DWORD*)((BYTE*)lib + export->AddressOfNames);
+
+    DWORD i = 0;
+    for(i; i < export->NumberOfNames; i++) {
+		    printf("%s\n",(CHAR*)lib + name[i]);
+	  }    
+}
+
+int main (int argc, char **argv) {
+    CHAR *dll = argv[1];
+    HANDLE hDll = LoadLibrary(dll);
+	
+    printf("Loading %s\\n", dll);
+    if(hDll == NULL) {
+        ExitProcess(0);
+    }
+
+	  DumpListOfExport(hDll);
+
+    CloseHandle(hDll);
+    return 0;
+}
